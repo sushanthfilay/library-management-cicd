1d322585c79389e69252096d491c747ec679a90e
sushanthfilay <sushanthfilay@gmail.com>
2025-03-12 21:05:08 +0530

    Add GitHub Actions CI/CD

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ijkjr\Downloads\Django-librarymanagement-main\Django-librarymanagement-main\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
